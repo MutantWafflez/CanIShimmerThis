d4a0bcb25041235545a646e77217ac5fb1f69932
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2025-08-28 21:45:35 -0700

    Add support for decrafting

--- Common/Configs/CISTModConfig.cs
+++ Common/Configs/CISTModConfig.cs
@@ -1,9 +1,16 @@
 ﻿using System.ComponentModel;
 using Terraria.ModLoader.Config;
 
-namespace CanIShimmerThis.Common.Configs; 
+namespace CanIShimmerThis.Common.Configs;
 
 public class CISTModConfig : ModConfig {
+    /// <summary>
+    ///     Whether or not "discovery mode" is enabled, where tooltips only show the transmutation
+    ///     outcome when the player has shimmered the given item previously.
+    /// </summary>
+    [DefaultValue(true)]
+    public bool discoveryMode;
+
     /// <summary>
     ///     Whether or not, if a given item can be shimmered, the tooltip will show what
     ///     the given item will shimmer/transmute into. Overrides <see cref="discoveryMode" />.
@@ -12,11 +19,25 @@ public class CISTModConfig : ModConfig {
     public bool forceShowOutcome;
 
     /// <summary>
-    ///     Whether or not "discovery mode" is enabled, where tooltips only show the transmutation
-    ///     outcome when the player has shimmered the given item previously.
+    ///     Whether to display the additional tooltip that reveals what a given item will decraft into,
+    ///     if it is shimmerable.
+    /// </summary>
+    [DefaultValue(false)]
+    public bool showDecrafts;
+
+    /// <summary>
+    ///     Whether or not, if a given item can be decrafted, the tooltip will show what
+    ///     the given item will decraft into. Overrides <see cref="discoveryMode" />.
+    /// </summary>
+    [DefaultValue(false)]
+    public bool forceShowDecraftOutcome;
+
+    /// <summary>
+    ///     Whether the decraft tooltip, when revealing the list of decraft items, will have all the items on
+    ///     one line (without their names).
     /// </summary>
     [DefaultValue(true)]
-    public bool discoveryMode;
+    public bool compactDecraftList;
 
     public override ConfigScope Mode => ConfigScope.ClientSide;
 }
--- Common/GlobalItems/TooltipGlobalItem.cs
+++ Common/GlobalItems/TooltipGlobalItem.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using CanIShimmerThis.Common.Configs;
 using CanIShimmerThis.Common.Players;
 using CanIShimmerThis.Common.Sets;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Enums;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
 
-namespace CanIShimmerThis.Common.GlobalItems; 
+namespace CanIShimmerThis.Common.GlobalItems;
 
 /// <summary>
 ///     Main class of the mod. Adds a tooltip line to each item that
@@ -18,6 +20,7 @@ namespace CanIShimmerThis.Common.GlobalItems;
 public class TooltipGlobalItem : GlobalItem {
     private static Item _shimmerItemDisplay;
     private static NPC _shimmerNPCDisplay;
+    private static readonly Color TooltipColor = new (218, 181, 229);
 
     public override void SetStaticDefaults() {
         _shimmerItemDisplay = new Item();
@@ -32,6 +35,14 @@ public class TooltipGlobalItem : GlobalItem {
         CISTModConfig config = ModContent.GetInstance<CISTModConfig>();
         DiscoveryPlayer discoveryPlayer = Main.LocalPlayer.GetModPlayer<DiscoveryPlayer>();
 
+        if (TryToApplyTransmutationTooltip(item, tooltips, config, discoveryPlayer)) {
+            return;
+        }
+
+        TryToApplyDecraftTooltip(item, tooltips, config, discoveryPlayer);
+    }
+
+    private bool TryToApplyTransmutationTooltip(Item item, List<TooltipLine> tooltips, CISTModConfig config, DiscoveryPlayer discoveryPlayer) {
         int shimmerEquivalentType = ItemID.Sets.ShimmerCountsAsItem[item.type] is not -1 and var countsAsType ? countsAsType : item.type;
         int itemTransformType = ItemID.Sets.ShimmerTransformToItem[shimmerEquivalentType];
         // Vanilla hard-code I can't do a whole lot about; so here it is repeated
@@ -64,25 +75,90 @@ public class TooltipGlobalItem : GlobalItem {
         }
 
         bool showTransmutationResult = config.forceShowOutcome || (config.discoveryMode && discoveryPlayer.DiscoveredShimmers.Contains(item));
-        string tooltipText = GetCISTTextValue("Shimmerable");
+
+        string defaultShimmerText = GetCISTTextValue("Shimmerable");
+        string transmuteText = null;
         if (itemTransformType != -1) {
             _shimmerItemDisplay.SetDefaults(itemTransformType);
-            tooltipText = showTransmutationResult ? GetCISTTextValue("ShimmerableIntoItem", itemTransformType, _shimmerItemDisplay.Name) : tooltipText;
+            transmuteText = showTransmutationResult ? GetCISTTextValue("ShimmerableIntoItem", itemTransformType, _shimmerItemDisplay.Name) : defaultShimmerText;
         }
         else if (npcTransformType != -1) {
             _shimmerNPCDisplay.SetDefaults(npcTransformType);
-            tooltipText = showTransmutationResult ? GetCISTTextValue("ShimmerableIntoNPC", _shimmerNPCDisplay.GivenOrTypeName) : tooltipText;
+            transmuteText = showTransmutationResult ? GetCISTTextValue("ShimmerableIntoNPC", _shimmerNPCDisplay.GivenOrTypeName) : defaultShimmerText;
         }
         else if (ItemID.Sets.CoinLuckValue[shimmerEquivalentType] is var coinLuck and > 0) {
-            tooltipText = GetCISTTextValue("ShimmerCoinLuck", $"+{coinLuck:##,###}");
+            transmuteText = GetCISTTextValue("ShimmerCoinLuck", $"+{coinLuck:##,###}");
         }
-        else {
+
+        if (transmuteText is null) {
+            return false;
+        }
+
+        tooltips.Add(
+            new TooltipLine(Mod, "CanBeShimmered", transmuteText) { OverrideColor = TooltipColor }
+        );
+
+        return true;
+    }
+
+    private void TryToApplyDecraftTooltip(Item item, List<TooltipLine> tooltips, CISTModConfig config, DiscoveryPlayer discoveryPlayer) {
+        if (!config.showDecrafts) {
+            return;
+        }
+
+        int decraftIndex = ShimmerTransforms.GetDecraftingRecipeIndex(item.type);
+        if (decraftIndex < 0) {
+            return;
+        }
+
+        string decraftText = GetDecraftText(config.forceShowDecraftOutcome || (config.discoveryMode && discoveryPlayer.DiscoveredShimmers.Contains(item)), decraftIndex, config);
+        if (decraftText is null) {
             return;
         }
 
         tooltips.Add(
-            new TooltipLine(Mod, "CanBeShimmered", tooltipText) { OverrideColor = new Color(218, 181, 229) }
+            new TooltipLine(Mod, "CanBeDecrafted", decraftText) { OverrideColor = TooltipColor }
         );
+    }
+
+    private string GetDecraftText(bool showDecraftResult, int decraftIndex, CISTModConfig config) {
+        string decraftText = GetCISTTextValue("Decraftable");
+        if (!showDecraftResult) {
+            return decraftText;
+        }
+
+        Recipe decraftRecipe = Main.recipe[decraftIndex];
+        List<Item> decraftOutcome = decraftRecipe.customShimmerResults ?? decraftRecipe.requiredItem;
+        if (decraftOutcome is null || decraftOutcome.Count == 0) {
+            return null;
+        }
+
+        StringBuilder builder = new();
+        if (!config.compactDecraftList) {
+            for (int i = 0; i < decraftOutcome.Count - 1; i++) {
+                Item decraftItem = decraftOutcome[i];
+
+                _shimmerItemDisplay.SetDefaults(decraftItem.type);
+                builder.AppendLine($"[i:{decraftItem.type}] ({_shimmerItemDisplay.Name})");
+            }
+
+            Item lastItem = decraftOutcome[^1];
+
+            _shimmerItemDisplay.SetDefaults(lastItem.type);
+            builder.Append($"[i:{lastItem.type}] ({_shimmerItemDisplay.Name})");
+
+            decraftText = GetCISTTextValue("DecraftsInto", builder.ToString());
+        }
+        else {
+            foreach (Item decraftItem in decraftOutcome) {
+                _shimmerItemDisplay.SetDefaults(decraftItem.type);
+                builder.Append($"[i:{decraftItem.type}]");
+            }
+
+            decraftText = GetCISTTextValue("DecraftsIntoCompact", builder.ToString());
+        }
+
+        return decraftText;
     }
 
     private string GetCISTTextValue(string suffix, params object[] args) => Language.GetTextValue($"Mods.CanIShimmerThis.{suffix}", args);
